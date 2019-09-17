af1eaa4962348126a8f9897f896c426cf188f5e9
Henrique Pereira <henriquepbdev@gmail.com>
2019-09-17 12:22:11 -0300

    Criado branch para atualizar para o react16

---
+++ frontend-old/src/billingCycle/billingCycle.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import ContentHeader from '../common/template/contentHeader';
+import Content from '../common/template/content';
+import Tabs from '../common/tab/tabs';
+import TabsHeader from '../common/tab/tabsHeader';
+import TabsContent from '../common/tab/tabsContent';
+import TabHeader from '../common/tab/tabHeader';
+import TabContent from '../common/tab/tabContent';
+import List from './billingCycleList';
+import Form from './billingCycleForm';
+
+import { init, create, update, remove } from './billingCycleActions';
+
+class BillingCycle extends Component {
+
+  componentWillMount() {
+    this.props.init();
+  }
+
+  render() {
+    return (
+      <div>
+        <ContentHeader title="Ciclo de Pagamentos" small="Cadastro" />
+        <Content>
+          <Tabs>
+            <TabsHeader>
+              <TabHeader label="Listar" icon="bars" target="tabList" />
+              <TabHeader label="Incluir" icon="plus" target="tabCreate" />
+              <TabHeader label="Alterar" icon="pencil" target="tabUpdate" />
+              <TabHeader label="Excluir" icon="trash-o" target="tabDelete" />
+            </TabsHeader>
+            <TabsContent>
+              <TabContent id="tabList"><List /></TabContent>
+              <TabContent id="tabCreate"><Form onSubmit={this.props.create}
+                submitClass="primary" submitLabel="Incluir" /></TabContent>
+              <TabContent id="tabUpdate"><Form onSubmit={this.props.update}
+                submitClass="info" submitLabel="Alterar" /></TabContent>
+              <TabContent id="tabDelete"><Form onSubmit={this.props.remove}
+                submitClass="danger" submitLabel="Excluir" readOnly={true} /></TabContent>
+            </TabsContent>
+          </Tabs>
+        </Content>
+      </div>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => bindActionCreators({ init, create, update, remove }, dispatch);
+export default connect(null, mapDispatchToProps)(BillingCycle);
--- frontend-old/src/main/app.jsx
+++ frontend-old/src/main/app.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
 import Header from '../common/template/header';
 import SideBar from '../common/template/sideBar';
 import Footer from '../common/template/footer';
 import Messages from '../common/msg/messages';
 
-import Routes from './routes';
-
 export default props => (
-  <HashRouter>
-    <div className="wrapper">
-      <Header />
-      <SideBar />
-      <Routes />
-      <Footer />
-      <Messages />
+  <div className="wrapper">
+    <Header />
+    <SideBar />
+    <div className="content-wrapper">
+      {props.children}
     </div>
-  </HashRouter>
+    <Footer />
+    <Messages />
+  </div>
 )
